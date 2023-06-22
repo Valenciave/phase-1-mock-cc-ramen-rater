fa3409e148be781945320072fc1a926a248c38fb
Fernando Valencia <fvalencia675@gmail.com>
2023-06-22 12:41:40 -0700

    mockcc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+document.addEventListener("DOMContentLoaded", () => {
+    fetch ("http://localhost:3000/ramens")
+        .then(response => response.json())
+        .then((data) => handleData(data))
+});
+
+
+
+
+function handleData(data) {
+    console.log(data)
+
+
+
+
+}
