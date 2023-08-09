29481e2e393210e40c7edd20053efaed6179128c
Fernando Valencia <fvalencia675@gmail.com>
2023-08-09 10:31:13 -0700

    images render in menu

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-// write your code here
-document.addEventListener("DOMContentLoaded", () => {
-    fetch ("http://localhost:3000/ramens")
-        .then(response => response.json())
-        .then((data) => handleData(data))
-});
+const ramenUrl = `http://localhost:3000/ramens`
 
+function getRamen(apiLink) {
+    return fetch(ramenUrl).then((r) => r.json()) };
+    
+const ramenResponse = getRamen(ramenUrl)
+ramenResponse.then((ramen) => {
+    for (let i = 0; i < ramen.length; i++) {
+        const ramenMenu = document.querySelector("#ramen-menu");
+        const image = document.createElement("img");
+        image.src = ramen[i].image;
+        
+        ramenMenu.append(image);
 
-
-
-function handleData(data) {
-    console.log(data)
-
-
-
-
+        //console.log(ramen[i])
 }
+});
